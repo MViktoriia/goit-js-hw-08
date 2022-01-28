1ecd59622e8de021bd6e1de5b58f0a702b8d0c98
Viktoriia Mykhalova <mihaleva.victoria@gmail.com>
2022-01-29 00:03:48 +0200

    adds task1-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle'
+    
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+playFromSavedSecond();
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+
+
+function onTimeUpdate(event) {
+     
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(event.seconds));
+    
+
+};
+
+function playFromSavedSecond() {
+    player.setCurrentTime(currentTime).then(function(seconds) {    // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+           case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+             break;
+
+           default:
+            // some other error occurred
+             break;
+    }
+});
+}
+
+
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle'
+
+const feedbackFormRef = document.querySelector(".feedback-form");
+
+
+feedbackFormRef.addEventListener("input", throttle(onFormInput, 500));
+feedbackFormRef.addEventListener("submit", onFormSubmit)
+
+populateForm();
+
+function onFormInput(event) {
+    console.log(event.currentTarget);
+    
+    if (event.currentTarget) {
+        localStorage.setItem("feedback-form-state", JSON.stringify({ email: event.currentTarget.email.value, message: event.currentTarget.message.value }));
+    };
+
+    
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.removeItem("feedback-form-state");
+    event.currentTarget.reset();
+
+};
+
+function populateForm() {
+    const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+    if (savedFormData) {
+        console.log(savedFormData);
+        feedbackFormRef
+        console.dir(feedbackFormRef);
+        feedbackFormRef.elements.email.value = savedFormData.email;
+        feedbackFormRef.elements.message.value = savedFormData.message;
+    }
+
+};
 

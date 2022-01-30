956bd0babe3ff03f5c5632b9a558a23cc971496d
Viktoriia Mykhalova <mihaleva.victoria@gmail.com>
2022-01-30 20:22:39 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,59 @@
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 
-const feedbackFormRef = document.querySelector(".feedback-form");
-// const submitBtn = document.querySelector("button");
-// console.log(submitBtn);
-// submitBtn.disabled = true;
+const STORAGE_KEY = "feedback-form-state";
 
-feedbackFormRef.addEventListener("input", throttle(onFormInput, 500));
-feedbackFormRef.addEventListener("submit", onFormSubmit)
+const formData = {
+
+};
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+}
+
+refs.form.addEventListener("input", throttle(onFormInput, 500));
+refs.form.addEventListener("submit", onFormSubmit)
 
 populateForm();
 
 function onFormInput(event) {
-           
-    if (event.currentTarget) {
-        localStorage.setItem("feedback-form-state", JSON.stringify({ email: event.currentTarget.email.value, message: event.currentTarget.message.value }));
-       
-        
-    };
+    console.log(event.target.name);
+    console.log(event.target.value);
+
+    formData[event.target.name] = event.target.value;
+    console.log(formData);
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+   
     
 };
 
 function onFormSubmit(event) {
     event.preventDefault();
-    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-    localStorage.removeItem("feedback-form-state");
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+    localStorage.removeItem(STORAGE_KEY);
     event.currentTarget.reset();
 
 };
 
 function populateForm() {
-    const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if (savedFormData) {
+        if (savedFormData.email) {
+            
+            refs.form.elements.email.value = savedFormData.email;
+        }
+
+        if (savedFormData.message) {
                         
-        feedbackFormRef.elements.email.value = savedFormData.email;
-        feedbackFormRef.elements.message.value = savedFormData.message;
+            refs.form.elements.message.value = savedFormData.message;
+        }
+
     }
 
+    
+   
 };
 
